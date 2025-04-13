f8b27e74ccc2ccc73a7f6a94ceb5134f8f9d58fb
Ludymilla Hableim <botelholudy@gmail.com>
2025-04-13 16:57:28 -0300

    Adicionando arquivos da pratica03

---
+++ praticas/pratica03/App.js
@@ -0,0 +1,10 @@
+import GalleryScreen from "./screens/GalleryScreen";
+import HomeScreen from "./screens/HomeScreen";
+import ListScreen from "./screens/ListScreen";
+import ProfileScreen from "./screens/ProfileScreen";
+
+function App() {
+  return <ProfileScreen />;
+}
+
+export default App;
